7afc3dbac888755ebae733095203776b3c3ed393
Parth Mangtani <impmmmm@gmail.com>
2025-07-19 06:53:52 +0530

    :sparkles: add update_profile_details_v0 endpoint.

--- square_common_bl/routes/authentication.py
+++ square_common_bl/routes/authentication.py
@@ -693,3 +693,71 @@ async def send_verification_email_v0(
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
         )
+
+
+@router.patch("/update_profile_details/v0")
+@global_object_square_logger.auto_logger()
+async def update_profile_details_v0(
+    access_token: Annotated[str, Header()],
+    first_name: Optional[str] = None,
+    last_name: Optional[str] = None,
+    email: Optional[str] = None,
+    phone_number_country_code: Optional[str] = None,
+    phone_number: Optional[str] = None,
+):
+    try:
+        """
+        validation
+        """
+        # pass
+        """
+        main process
+        """
+        response = global_object_square_authentication_helper.update_profile_details_v0(
+            access_token=access_token,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            phone_number_country_code=phone_number_country_code,
+            phone_number=phone_number,
+        )
+        """
+        return value
+        """
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response,
+        )
+    except HTTPError as http_error:
+        global_object_square_logger.logger.error(http_error, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_error.response.status_code,
+            content=json.loads(http_error.response.content),
+        )
+    except HTTPException as http_exception:
+        global_object_square_logger.logger.error(http_exception, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_exception.status_code, content=http_exception.detail
+        )
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"],
+            log=str(e),
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
